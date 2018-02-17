0ca63dc5458650cb89f76a23bc8303ec0acc9376
NickDErrico <nickderrico@hotmail.com>
2018-02-17 14:07:15 -0700

    simple youtube clone complete

---
+++ src/components/Video_List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VideoListItem from './Video_List_Item';
+
+const VideoList = (props) => {
+const videoItems = props.videos.map((video) => {
+  return (
+    <VideoListItem
+      key={video.etag}
+      video={video}
+      onVideoSelect={props.onVideoSelect}
+    />
+  )
+})
+
+  return (
+    <ul className="col-md-4 group">
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/Search_Bar'
+import VideoDetail from './components/Video_Detail'
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/Video_List'
+const API_KEY = 'AIzaSyAGdQ8GXQlCkJUASIY9FpHKEtTW5yIZdRI'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
